0b18fa9fe1c8a14c608de79efafb5ad8e6041243
Autumn Charles <autumn.m.s.c@gmail.com>
2024-11-23 13:56:13 -0500

    edited the php file to add the html table to show the additional results in

--- world.php
+++ world.php
@@ -18,11 +18,30 @@ $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<!--
 <ul>
 <?php foreach ($results as $row): ?>
-	<!--
+
 	<li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-	-->
+	
 	<li><?= htmlspecialchars($row['name']) . ' is ruled by ' . htmlspecialchars($row['head_of_state']); ?></li>
 <?php endforeach; ?>
 </ul>
+-->
+
+<table>
+	<tr>
+		<th><?='Name';?></th>
+		<th><?='Continent';?></th>
+		<th><?='Independence';?></th>
+		<th><?='Head of State';?></th>
+	</tr>
+	<?php foreach ($results as $row): ?>
+	<tr>
+		<td><?=$row['name'];?></td>
+		<td><?=$row['continent'];?></td>
+		<td><?=$row['independence_year'];?></td>
+		<td><?=$row['head_of_state'];?></td>
+	</tr>
+	<?php endforeach; ?>
+</table>
